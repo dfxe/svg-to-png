34bf2419a8ff1667dddb005134ac96e97611498f
caravagio <62392399+caravagio@users.noreply.github.com>
2022-05-18 04:51:14 +0100

    Add rounded corners noise canvas

--- svg-to-image/components/NoiseCanvas.tsx
+++ svg-to-image/components/NoiseCanvas.tsx
@@ -11,11 +11,69 @@ function NoiseCanvas() {
   const generateImages = useCallback(() => {
     if (canvas) {
       //set width and height of image to 100
-      const commonContext = canvas.getContext("2d");
+      const ctx = canvas.getContext("2d");
       //best quality
-      commonContext!.imageSmoothingEnabled = true;
-      commonContext!.imageSmoothingQuality = "high";
-
+      ctx!.imageSmoothingEnabled = true;
+      ctx!.imageSmoothingQuality = "high";
+      const roundRect = (ctx, x, y, width, height, radius, fill, stroke) => {
+        if (typeof stroke === "undefined") {
+          stroke = true;
+        }
+        if (typeof radius === "undefined") {
+          radius = 5;
+        }
+        if (typeof radius === "number") {
+          radius = { tl: radius, tr: radius, br: radius, bl: radius };
+        } else {
+          var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+          for (var side in defaultRadius) {
+            radius[side] = radius[side] || defaultRadius[side];
+          }
+        }
+        ctx.beginPath();
+        ctx.moveTo(x + radius.tl, y);
+        ctx.lineTo(x + width - radius.tr, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+        ctx.lineTo(x + width, y + height - radius.br);
+        ctx.quadraticCurveTo(
+          x + width,
+          y + height,
+          x + width - radius.br,
+          y + height
+        );
+        ctx.lineTo(x + radius.bl, y + height);
+        ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+        ctx.lineTo(x, y + radius.tl);
+        ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+        ctx.closePath();
+        if (fill) {
+          ctx.fill();
+        }
+        if (stroke) {
+          ctx.stroke();
+        }
+      };
+      roundRect(ctx, 5, 5, 50, 50);
+      // To change the color on the rectangle, just manipulate the context
+      ctx!.strokeStyle = "rgb(255, 0, 0)";
+      ctx!.fillStyle = "rgba(255, 255, 0, .5)";
+      roundRect(ctx, 100, 5, 100, 100, 20, true);
+      // Manipulate it again
+      ctx!.strokeStyle = "#0f0";
+      ctx!.fillStyle = "#ddd";
+      // Different radii for each corner, others default to 0
+      roundRect(
+        ctx,
+        300,
+        5,
+        200,
+        100,
+        {
+          tl: 50,
+          br: 25,
+        },
+        true
+      );
       //maybe wait a bit before generating background
       //generateBackground(commonContext!, 400, 400);
       /* drawGrid(
